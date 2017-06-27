1678a0e0c7b3862933704a5558cb518e753d1f09
Juan Carlos Orozco <jorozconews@gmail.com>
2017-06-27 12:57:14 -0500

    Change source to ../blockpy/blockly assumes that blockpy is installed with npm

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ var _browserRename = function(path) {
 }
 
 gulp.task('blockly', function() {
-  return gulp.src('blockly/blockly_compressed.js')
+  return gulp.src('../blockpy/blockly/blockly_compressed.js')
       .pipe(replace(/goog\.global\s*=\s*this;/, 'goog.global=global;'))
       .pipe(insert.wrap(`
       var DOMParser = require("xmldom").DOMParser; 
@@ -38,7 +38,7 @@ gulp.task('blockly', function() {
 });
 
 gulp.task('blockly_browser', function() {
-  return gulp.src('blockly/blockly_compressed.js')
+  return gulp.src('../blockpy/blockly/blockly_compressed.js')
       .pipe(replace(/goog\.global\s*=\s*this;/, 'goog.global=window;'))
       .pipe(insert.wrap(`
       module.exports = (function(){`,
@@ -50,7 +50,7 @@ gulp.task('blockly_browser', function() {
 });
 
 gulp.task('blocks', function() {
-  return gulp.src('blockly/blocks_compressed.js')
+  return gulp.src('../blockpy/blockly/blocks_compressed.js')
       .pipe(insert.wrap(`
         module.exports = function(Blockly){
           var goog = Blockly.goog;
@@ -63,7 +63,7 @@ gulp.task('blocks', function() {
 });
 
 gulp.task('blocks_browser', function() {
-  return gulp.src('blockly/blocks_compressed.js')
+  return gulp.src('../blockpy/blockly/blocks_compressed.js')
       .pipe(insert.wrap(`
         module.exports = function(Blockly){
           var goog = Blockly.goog;
@@ -76,38 +76,38 @@ gulp.task('blocks_browser', function() {
 });
 
 gulp.task('js', function() {
-  return gulp.src('blockly/javascript_compressed.js')
+  return gulp.src('../blockpy/blockly/javascript_compressed.js')
       .pipe(insert.wrap('module.exports = function(Blockly){', 'return Blockly.JavaScript;}'))
       .pipe(gulp.dest('lib'))
 });
 
 gulp.task('dart', function() {
-  return gulp.src('blockly/dart_compressed.js')
+  return gulp.src('../blockpy/blockly/dart_compressed.js')
       .pipe(insert.wrap('module.exports = function(Blockly){', 'return Blockly.Dart;}'))
       .pipe(replace(/window\./g, ''))
       .pipe(gulp.dest('lib'))
 });
 
 gulp.task('python', function() {
-  return gulp.src('blockly/python_compressed.js')
+  return gulp.src('../blockpy/blockly/python_compressed.js')
       .pipe(insert.wrap('module.exports = function(Blockly){', 'return Blockly.Python;}'))
       .pipe(gulp.dest('lib'))
 });
 
 gulp.task('php', function() {
-  return gulp.src('blockly/php_compressed.js')
+  return gulp.src('../blockpy/blockly/php_compressed.js')
       .pipe(insert.wrap('module.exports = function(Blockly){', 'return Blockly.PHP;}'))
       .pipe(gulp.dest('lib'))
 });
 
 gulp.task('lua', function() {
-  return gulp.src('blockly/lua_compressed.js')
+  return gulp.src('../blockpy/blockly/lua_compressed.js')
       .pipe(insert.wrap('module.exports = function(Blockly){', 'return Blockly.Lua;}'))
       .pipe(gulp.dest('lib'))
 });
 
 gulp.task('en', function() {
-  return gulp.src('blockly/msg/js/en.js')
+  return gulp.src('../blockpy/blockly/msg/js/en.js')
       .pipe(replace(/goog\.[^\n]+/g, ''))
       .pipe(insert.wrap('var Blockly = {}; Blockly.Msg={};  module.exports = function(){ ', 'return Blockly.Msg;}'))
       .pipe(gulp.dest('lib/i18n/'))
